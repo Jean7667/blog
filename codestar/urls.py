fd9e53d47a0cd8089f2244a28f43bfdb8fc5bc15
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-02-20 17:09:06 +0000

    initial deployment of django with virtenv

--- codestar/urls.py
+++ codestar/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for blog project.
+URL configuration for codestar project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello_world import views as index_views
+from blog import views as blog_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path("blog/", blog_views.my_blog, name='blog'),
     path('admin/', admin.site.urls),
 ]
