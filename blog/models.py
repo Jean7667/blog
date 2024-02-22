ee586e27c5b81aa87c9ef08433f085fde7ac0ed1
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-02-22 11:45:42 +0000

    added in models (Methods for post and comments) and mkmigr + migr

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,25 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+# Meta class is used to specify the default ordering of records retrieved from the database for this model.
+class Meta:
+    ordering = ["-created_on"]
+# remember self is convention for readability not a keyword
+def __str__(self):
+    return f"{self.title} | written by {self.author}"
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+class Meta:
+        ordering = ["created_on"]
+# remember self is convention for readability not a keyword
+def __str__(self):
+    return f"Comment {self.body} by {self.name}"
+    
