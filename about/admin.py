6a6c07bbb71baf752d0bb6a4af6da3c603189743
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-02-26 17:34:49 +0000

    collaboration app part form completed

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,8 @@
+# package in Django that contains a collection of reusable Django applications and modules provided by the Django project
 from django.contrib import admin
+
 from .models import About
+from .models import CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -8,3 +11,9 @@ from django_summernote.admin import SummernoteModelAdmin
 class AboutAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
+# to customise the admin panel view in your own projects, then inherit from admin.ModelAdmin
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)    
